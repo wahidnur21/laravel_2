6ba40710fe19f4d65817244c3212d36d3da76ffb
ihsann <ihsann820@gmail.com>
2024-05-01 19:39:41 +0700

    selesai

--- resources/views/customer/list.blade.php
+++ resources/views/customer/list.blade.php
@@ -14,7 +14,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="#">Homme</a></li>
               <li class="breadcrumb-item active">Data Customer</li>
             </ol>
           </div>
